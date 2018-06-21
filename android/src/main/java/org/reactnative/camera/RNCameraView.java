bc98b67063e5749dd543905ee2fb666b29037e0f
snehesht <mail@snehesh.me>
2018-06-21 11:17:17 -0400

    Added getHlsPath

--- android/src/main/java/org/reactnative/camera/RNCameraView.java
+++ android/src/main/java/org/reactnative/camera/RNCameraView.java
@@ -200,6 +200,12 @@ public class RNCameraView extends CameraView implements LifecycleEventListener, 
   }
 
   @Override
+  public void getHlsPath() {
+    // Returns the tmp directory of the recorded segments
+    return getContext().getCacheDir() + "/Camera";
+  }
+
+  @Override
   protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
     View preview = getView();
     if (null == preview) {
