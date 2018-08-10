7e24b4f72594329d218e399f7210676a1d89d679
Dan Goldstein <dan@axelby.com>
2018-08-10 11:23:19 -0500

    remove unused import

--- android/src/main/java/org/reactnative/camera/RNCameraView.java
+++ android/src/main/java/org/reactnative/camera/RNCameraView.java
@@ -5,7 +5,6 @@ import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Build;
 import android.support.v4.content.ContextCompat;
-import android.util.Log;
 import android.util.SparseArray;
 import android.view.View;
 
